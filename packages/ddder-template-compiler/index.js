47d89b242795db662ec0f19decd784dd4e3ed73b
SamHwang1990 <samhwang1990@gmail.com>
2017-11-02 15:19:44 +0800

    chore: Release v2.4.4-ddder.4

--- packages/ddder-template-compiler/index.js
+++ packages/ddder-template-compiler/index.js
@@ -1,13 +1,13 @@
 try {
-  var vueVersion = require('@ddder/vue').version
+  var vueFactoryVersion = require('@ddder/vue-framework').version
 } catch (e) {}
 
 var packageName = require('./package.json').name
 var packageVersion = require('./package.json').version
-if (vueVersion && vueVersion !== packageVersion) {
+if (vueFactoryVersion && vueFactoryVersion !== packageVersion) {
   throw new Error(
     '\n\nVue packages version mismatch:\n\n' +
-    '- vue@' + vueVersion + '\n' +
+    '- vue@' + vueFactoryVersion + '\n' +
     '- ' + packageName + '@' + packageVersion + '\n\n' +
     'This may cause things to work incorrectly. Make sure to use the same version for both.\n' +
     'If you are using vue-loader@>=10.0, simply update vue-template-compiler.\n' +
